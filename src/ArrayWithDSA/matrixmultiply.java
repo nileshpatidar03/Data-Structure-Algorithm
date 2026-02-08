dbf9f7942d745abd8e8008cb70fa28c7c4b0ee48
codernilesh <coders@gamil.com>
2026-02-09 00:28:52 +0530

    matrix multiply

---
+++ src/ArrayWithDSA/matrixmultiply.java
@@ -0,0 +1,48 @@
+package ArrayWithDSA;
+
+public class matrixmultiply
+{
+    public static int [][]  multiply(int [][] A, int [][] B)
+    {
+        int n = A.length;
+        int [][] C = new int [n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                for(int k=0;k<n;k++)
+                {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+       return C;
+    }
+
+            public static void main(String [] args )
+            {
+                int[][]A = {
+                        { 1,2},
+                        {3,4}
+                };
+                int [][] B = {
+                        {5,6},
+                        {7,8}
+                };
+                int [][] result =matrixmultiply.multiply(A,B);
+               // System.out.println(result);
+                for(int i = 0; i < result.length; i++)
+                {
+                    for(int j = 0; j < result[0].length; j++)
+                    {
+                        System.out.print(result[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+
+            }
+
+
+
+
+}
